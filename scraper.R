76ffd203c30aedad6765a36edc7f16867a79e7f7
miguelito-git <63246434+miguelito-git@users.noreply.github.com>
2020-07-23 01:48:33 +0200

    Scrapper & playlists exported
    Only song and artist names. No music genre yet.

--- scraper.R
+++ scraper.R
@@ -10,24 +10,30 @@ start_date = as_date('2005-01-01')
 today = lubridate::today()-as.period(1,'week')
 date = start_date
 n_weeks = as.numeric(floor((today-start_date)/7))
+
+playlists <- as.list(1:n_weeks)
+  
 pb <- progress_bar$new(total=n_weeks)
-for (k in 1:n_weeks) {
+for (k in 610:n_weeks) {
   year = year(date)
   week = week(date)
   url = make_url(year, week)
   los40 <- read_html(url)
   listwrapper <- html_nodes(los40, '.info_grupo')
   # we now get the songs and artists
-  top40 <- as.data.frame(matrix(ncol = 3, nrow = 40))
-  colnames(top40) <- c('Song', 'Artist', 'Genre')
-  for(i in 1:40){
+  top40 <- as.data.frame(matrix(ncol = 2, nrow = 40))
+  colnames(top40) <- c('Song', 'Artist')
+  l <- length(listwrapper)
+  for(i in 1:l){
     item = listwrapper[[i]]
     song <- html_text(html_node(item, 'p'))
     artist <- html_text(html_node(item, 'h4 a'))
-    genre <- ''  #  do something to get the genres
-    top40[i,] <- data.frame(song, artist, genre)
-    
+    top40[i,] <- data.frame(song, artist)
   }
+  playlists[[k]] <- top40
+  write.csv(top40, file = paste('./playlists/', year,'-',week,'.csv', sep = ''))
   date = date + as.period(1,'week')
   pb$tick()
 }
+saveRDS(playlists, file='listofplaylists.RData')
+readRDS('listofplaylists.RData')
